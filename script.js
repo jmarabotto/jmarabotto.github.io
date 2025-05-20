45d81d278ca1165351a7c9eff7aadbd1bbfb2564
Julien Marabotto <julienmarabotto@MBP-de-Henri-2.lan>
2025-05-20 18:15:43 +0200

    build: externalise script to .js file (sorry Toby). Define header in header.html, called in each html page using script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+/* fetch the header html file to show header */
+fetch('header.html') // adjust path if in a subfolder
+    .then(res => res.text())
+    .then(data => {
+      document.getElementById('header').innerHTML = data;
+    }
+)
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+    
+/* Close the dropdown if the user clicks outside of it */
+window.onclick = function(e) {
+    if (!e.target.matches('.dropbtn')) {
+    var myDropdown = document.getElementById("myDropdown");
+    if (myDropdown.classList.contains('show')) {
+        myDropdown.classList.remove('show');
+        }
+    }
+}
